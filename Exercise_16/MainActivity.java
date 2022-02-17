e8f0d140028c5ba48018b3f51c4ebd99c3724b3c
Anupama-Regi <74719646+Anupama-Regi@users.noreply.github.com>
2022-02-17 21:38:07 +0530

    Update MainActivity.java

--- Exercise_16/MainActivity.java
+++ Exercise_16/MainActivity.java
@@ -1,13 +1,8 @@
 package com.example.exercise_16;
 
 import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
-
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.content.Intent;
-import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
